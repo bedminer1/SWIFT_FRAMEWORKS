f874c08bdd618cdcfe17b547c83353cbc85e1827
bedminer1 <alexander.seah.min.rui@gmail.com>
2025-03-03 12:34:47 +0800

    init

---
+++ Apple_Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple_Frameworks
+//
+//  Created by Alex on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
